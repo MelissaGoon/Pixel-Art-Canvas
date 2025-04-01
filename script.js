42429f220ee2b871b0bdde958ddc4a715c4bf3b7
MelissaGoon <melissagoon13743@gmail.com>
2025-04-01 10:43:59 -0700

    Added basic colour change and some styling

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const block_container = document.querySelector(".block-container")
 for (let i = 0; i < (16*16); i++) {
     const block = document.createElement("div");
     block.classList.toggle("block");
-    block_container.appendChild(block)
+    block.addEventListener("mouseover", () => {
+        block.style.backgroundColor = 'white';
+    });
+    block_container.appendChild(block);
 
 }
