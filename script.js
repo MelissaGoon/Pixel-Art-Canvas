67dc7e2bcf089eb97a66f2d7f5a9e225af00f668
MelissaGoon <melissagoon13743@gmail.com>
2025-04-01 11:10:17 -0700

    Added title, changed styling and grid creation logic
    Updated logic for setting width of the grid to account for variable size of grid.

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const block_container = document.querySelector(".block-container")
 
-for (let i = 0; i < (16*16); i++) {
-    const block = document.createElement("div");
-    block.classList.toggle("block");
-    block.addEventListener("mouseover", () => {
-        block.style.backgroundColor = 'white';
-    });
-    block_container.appendChild(block);
 
+function createGrid(n) {
+    const block_container = document.querySelector(".block-container")
+    let width = String((16 * (n+1)) + (1 * (n + 1)));
+    block_container.style.width = width + "px";
+
+    for (let i = 0; i < (n * n); i++) {
+        const block = document.createElement("div");
+        block.classList.toggle("block");
+        block.addEventListener("mouseover", () => {
+            block.style.backgroundColor = 'white';
+        });
+        block_container.appendChild(block);
+
+    }
 }
+
+createGrid(17);
