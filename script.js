9bf58e9fbe0f2cf9bf5a6404e7db7a980b355353
MelissaGoon <melissagoon13743@gmail.com>
2025-04-02 16:16:19 -0700

    Finished random colour

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function changeColor(block, event) {
             block.style.backgroundColor = 'white';
         } else if (random_active) {
             const randomColor = Math.floor(Math.random()*16777215).toString(16);
-            block.style.backgroundColor = randomColor;
+            block.style.backgroundColor = "#" + randomColor;
 
         } else {
             block.style.backgroundColor = 'rgb(11, 0, 59)';
